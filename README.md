1b310d54ef6651182bf8cb1e1316199ddc106ae7
Cheyne <cheyne.hammett@gmail.com>
2019-10-20 17:26:53 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# basic-c-sdl-game-nx
+Switch port of a basic SDL2 "game" originally created by Amine B. Hassouna
+
+
+## License
+
+**Original Author**
+
+Amine B. Hassouna [@aminosbh](https://gitlab.com/aminosbh)
+
+https://github.com/aminosbh/basic-c-sdl-game
---
+++ src/main.c
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2018, 2019 Amine Ben Hassouna <amine.benhassouna@gmail.com>
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any
+ * person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the
+ * Software without restriction, including without
+ * limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice
+ * shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+ * ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+ * SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ * IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#include <SDL.h>
+#include <switch.h>
+#include "game.h"
+
+#define JOY_A     0
+#define JOY_B     1
+#define JOY_X     2
+#define JOY_Y     3
+#define JOY_MINUS 11
+#define JOY_LEFT  12
+#define JOY_UP    13
+#define JOY_RIGHT 14
+#define JOY_DOWN  15
+
+// Define screen dimensions
+#define SCREEN_WIDTH    1280
+#define SCREEN_HEIGHT   720
+
+int main(int argc, char* argv[])
+{
+
+    SDL_InitSubSystem(SDL_INIT_JOYSTICK);
+    SDL_JoystickEventState(SDL_ENABLE);
+    SDL_JoystickOpen(0);
+
+    // Unused argc, argv
+    (void) argc;
+    (void) argv;
+
+    // Initialize SDL
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        fprintf(stderr, "SDL could not be initialized!\n"
+                        "SDL_Error: %s\n", SDL_GetError());
+        return 0;
+    }
+
+#if defined linux && SDL_VERSION_ATLEAST(2, 0, 8)
+    // Disable compositor bypass
+    if(!SDL_SetHint(SDL_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR, "0"))
+    {
+        printf("SDL can not disable compositor bypass!\n");
+        return 0;
+    }
+#endif
+
+    // Create window
+    SDL_Window *window = SDL_CreateWindow("Basic C SDL game",
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          SCREEN_WIDTH, SCREEN_HEIGHT,
+                                          SDL_WINDOW_SHOWN);
+    if(!window)
+    {
+        fprintf(stderr, "Window could not be created!\n"
+                        "SDL_Error: %s\n", SDL_GetError());
+    }
+    else
+    {
+        // Create renderer
+        SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+        if(!renderer)
+        {
+            fprintf(stderr, "Renderer could not be created!\n"
+                            "SDL_Error: %s\n", SDL_GetError());
+        }
+        else
+        {
+            // Start the game
+            Game_start(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+            // Destroy renderer
+            SDL_DestroyRenderer(renderer);
+        }
+
+        // Destroy window
+        SDL_DestroyWindow(window);
+    }
+
+    // Quit SDL
+    SDL_Quit();
+
+    return 0;
+}
